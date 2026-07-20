7f2281d3da87bffafbe3dc1d82cf348967d9311f
Yiqun <niuyiqun8@gmail.com>
2026-07-20 20:56:57 +0800

    gai

--- MATLAB_simlink/第二章/chapter2.m
+++ MATLAB_simlink/第二章/chapter2.m
@@ -1,14 +1,14 @@
 
 %%  2.4 MATLAB 的程序流控制  %%
 %%  2.4.1 循环控制结构 %%
-% % for 循环结构 %%   这里和python的不同是需要有end结尾
-% sum = 0;
-% for i = 1:1:50  %循环变量等于向量表达式
-%     sum = sum + i;
-%     % fprintf('i 的值是 %.2f\n', i)
-%     % fprintf('求和结果：%.2f\n', sum)
-% end
-% disp(sum)
+% for 循环结构 %%   这里和python的不同是需要有end结尾
+sum = 0;
+for i = 1:1:50  %循环变量等于向量表达式
+    sum = sum + i;
+    fprintf('i 的值是 %.2f\n', i)
+    fprintf('求和结果：%.2f\n', sum)
+end
+disp(sum)
 
 
 %% while 循环结构 %%
